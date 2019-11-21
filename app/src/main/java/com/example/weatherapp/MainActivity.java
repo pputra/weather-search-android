e4554dad52be1b9464a5e0a59b7f0bd98d7b3d8d
pputra <mynameisyoga@live.com>
2019-11-21 15:50:25 -0800

    feat: add detail weather activity

--- app/src/main/java/com/example/weatherapp/MainActivity.java
+++ app/src/main/java/com/example/weatherapp/MainActivity.java
@@ -1,13 +1,20 @@
 package com.example.weatherapp;
 
+import android.content.Intent;
+import android.support.constraint.ConstraintLayout;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public void showDetailWeather(View v) {
+        Intent intentThatShowDetailWeatherActivity = new Intent(this, DetailWeatherActivity.class);
+        startActivity(intentThatShowDetailWeatherActivity);
     }
 }
