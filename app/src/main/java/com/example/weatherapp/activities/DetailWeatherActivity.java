d1ea73aa0d5c152cce263e471a1355ab2aaec6ca
pputra <mynameisyoga@live.com>
2019-11-23 00:55:01 -0800

    feat: add back button to detail activity action bar

--- app/src/main/java/com/example/weatherapp/activities/DetailWeatherActivity.java
+++ app/src/main/java/com/example/weatherapp/activities/DetailWeatherActivity.java
@@ -2,8 +2,10 @@ package com.example.weatherapp.activities;
 
 import android.support.design.widget.TabLayout;
 import android.support.v4.view.ViewPager;
+import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.MenuItem;
 
 import com.example.weatherapp.R;
 import com.example.weatherapp.adapters.ViewPagerAdapter;
@@ -24,5 +26,20 @@ public class DetailWeatherActivity extends AppCompatActivity {
         tabLayout = findViewById(R.id.tablayout_detail_tabs);
         tabLayout.setupWithViewPager(viewPager);
 
+        ActionBar actionBar = getSupportActionBar();
+        actionBar.setHomeButtonEnabled(true);
+        actionBar.setDisplayHomeAsUpEnabled(true);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // enable back button
+        switch (item.getItemId()) {
+            case android.R.id.home:
+                this.finish();
+                return true;
+            default:
+                return super.onOptionsItemSelected(item);
+        }
     }
 }
