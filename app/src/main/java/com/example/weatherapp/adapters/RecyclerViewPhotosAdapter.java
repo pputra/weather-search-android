b0ccf10c6b12f39c5b20cb3a52c004fc87c76a54
pputra <mynameisyoga@live.com>
2019-11-27 16:19:16 -0800

    feat: add photos tab using recycler view

---
+++ app/src/main/java/com/example/weatherapp/adapters/RecyclerViewPhotosAdapter.java
@@ -0,0 +1,52 @@
+package com.example.weatherapp.adapters;
+
+import android.support.annotation.NonNull;
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.LinearLayout;
+
+import com.example.weatherapp.R;
+import com.squareup.picasso.Picasso;
+
+import java.util.List;
+
+public class RecyclerViewPhotosAdapter extends RecyclerView.Adapter<RecyclerViewPhotosAdapter.PhotosViewHolder> {
+    private List<String> mPhotosUrlList;
+
+    public static class PhotosViewHolder extends RecyclerView.ViewHolder {
+        ImageView mImageViewCityPhoto;
+        public PhotosViewHolder(View v) {
+            super(v);
+
+            mImageViewCityPhoto = v.findViewById(R.id.image_city_photo);
+        }
+    }
+
+    public RecyclerViewPhotosAdapter(List<String> photosUrlList) {
+        mPhotosUrlList = photosUrlList;
+    }
+
+    @NonNull
+    @Override
+    public PhotosViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int i) {
+        LinearLayout v = (LinearLayout) LayoutInflater.from(parent.getContext()).inflate(R.layout.photo_list_item, parent, false);
+
+        PhotosViewHolder vh = new PhotosViewHolder(v);
+
+        return vh;
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull PhotosViewHolder photosViewHolder, int i) {
+        String url = mPhotosUrlList.get(i);
+        Picasso.get().load(url).into(photosViewHolder.mImageViewCityPhoto);
+    }
+
+    @Override
+    public int getItemCount() {
+        return mPhotosUrlList.size();
+    }
+}
--- app/src/main/java/com/example/weatherapp/fragments/PhotosFragment.java
+++ app/src/main/java/com/example/weatherapp/fragments/PhotosFragment.java
@@ -3,20 +3,22 @@ package com.example.weatherapp.fragments;
 
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.TextView;
 
 import com.example.weatherapp.R;
-
-import org.w3c.dom.Text;
+import com.example.weatherapp.adapters.RecyclerViewPhotosAdapter;
+import com.example.weatherapp.models.WeatherDetail;
 
 /**
  * A simple {@link Fragment} subclass.
  */
 public class PhotosFragment extends Fragment {
-    private TextView mTextViewMessage;
+    RecyclerView mRecyclerViewPhotos;
+    RecyclerViewPhotosAdapter mPhotosAdapter;
 
     public PhotosFragment() {
         // Required empty public constructor
@@ -29,8 +31,13 @@ public class PhotosFragment extends Fragment {
         // Inflate the layout for this fragment
         View view = inflater.inflate(R.layout.fragment_photos, container, false);
 
-        mTextViewMessage = view.findViewById(R.id.tv_photos_message);
-        mTextViewMessage.setText("This is PhotosFragment");
+        mRecyclerViewPhotos = view.findViewById(R.id.rv_photos);
+        mRecyclerViewPhotos.setLayoutManager(new LinearLayoutManager(getContext()));
+
+        WeatherDetail weatherDetail = (WeatherDetail) getArguments().getSerializable("WEATHER_DETAIL");
+
+        mPhotosAdapter = new RecyclerViewPhotosAdapter(weatherDetail.getPhotosUrlList());
+        mRecyclerViewPhotos.setAdapter(mPhotosAdapter);
 
         return view;
     }
