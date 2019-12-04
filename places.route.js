c0772d901c8616d94efed45fcad01faa2b52294a
pputra <mynameisyoga@live.com>
2019-12-03 19:05:10 -0800

    submit

--- places.route.js
+++
@@ -1,6 +0,0 @@
-const router = require('express').Router();
-const { getPlacesSuggestions } = require('../controllers/places.controller');
-
-router.get('/', getPlacesSuggestions);
-
-module.exports = router;
--- weather.route.js
+++
@@ -1,6 +0,0 @@
-const router = require('express').Router();
-const { getWeather } = require('../controllers/weather.controller');
-
-router.get('/', getWeather);
-
-module.exports = router;
